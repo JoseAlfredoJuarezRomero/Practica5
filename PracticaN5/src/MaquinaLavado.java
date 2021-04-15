6e1e226450015f31ab15f6f766b173df819da3f9
Alfredo Juarez <Alfredo Juarez@LAPTOP-DE473M91>
2021-04-15 12:26:11 -0500

    Prac5

---
+++ PracticaN5/src/MaquinaLavado.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of MaquinaLavado.
+ * 
+ * @author Alfredo Juarez
+ */
+public class MaquinaLavado {
+	/**
+	 * Description of the property capacidadLitros.
+	 */
+	private float capacidadLitros = 0F;
+
+	/**
+	 * Description of the property tiempoLavado.
+	 */
+	private float tiempoLavado = 0F;
+
+	/**
+	 * Description of the property costoLavado.
+	 */
+	private float costoLavado = 0F;
+
+	// Start of user code (user defined attributes for MaquinaLavado)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public MaquinaLavado() {
+		// Start of user code constructor for MaquinaLavado)
+		super();
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for MaquinaLavado)
+
+	// End of user code
+	/**
+	 * Returns capacidadLitros.
+	 * @return capacidadLitros 
+	 */
+	public float getCapacidadLitros() {
+		return this.capacidadLitros;
+	}
+
+	/**
+	 * Sets a value to attribute capacidadLitros. 
+	 * @param newCapacidadLitros 
+	 */
+	public void setCapacidadLitros(float newCapacidadLitros) {
+		this.capacidadLitros = newCapacidadLitros;
+	}
+
+	/**
+	 * Returns tiempoLavado.
+	 * @return tiempoLavado 
+	 */
+	public float getTiempoLavado() {
+		return this.tiempoLavado;
+	}
+
+	/**
+	 * Sets a value to attribute tiempoLavado. 
+	 * @param newTiempoLavado 
+	 */
+	public void setTiempoLavado(float newTiempoLavado) {
+		this.tiempoLavado = newTiempoLavado;
+	}
+
+	/**
+	 * Returns costoLavado.
+	 * @return costoLavado 
+	 */
+	public float getCostoLavado() {
+		return this.costoLavado;
+	}
+
+	/**
+	 * Sets a value to attribute costoLavado. 
+	 * @param newCostoLavado 
+	 */
+	public void setCostoLavado(float newCostoLavado) {
+		this.costoLavado = newCostoLavado;
+	}
+
+}
---
+++ PracticaN5/src/MaquinasEmpaquetado.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of MaquinasEmpaquetado.
+ * 
+ * @author Alfredo Juarez
+ */
+public class MaquinasEmpaquetado {
+	/**
+	 * Description of the property tipoEmpaques.
+	 */
+	private String tipoEmpaques = "";
+
+	/**
+	 * Description of the property catEmpaquesMin.
+	 */
+	private float catEmpaquesMin = 0F;
+
+	/**
+	 * Description of the property costoEmpaquetado.
+	 */
+	private float costoEmpaquetado = 0F;
+
+	/**
+	 * Description of the property costoBotellaMin.
+	 */
+	private float costoBotellaMin = 0F;
+
+	// Start of user code (user defined attributes for MaquinasEmpaquetado)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public MaquinasEmpaquetado() {
+		// Start of user code constructor for MaquinasEmpaquetado)
+		super();
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for MaquinasEmpaquetado)
+
+	// End of user code
+	/**
+	 * Returns tipoEmpaques.
+	 * @return tipoEmpaques 
+	 */
+	public String getTipoEmpaques() {
+		return this.tipoEmpaques;
+	}
+
+	/**
+	 * Sets a value to attribute tipoEmpaques. 
+	 * @param newTipoEmpaques 
+	 */
+	public void setTipoEmpaques(String newTipoEmpaques) {
+		this.tipoEmpaques = newTipoEmpaques;
+	}
+
+	/**
+	 * Returns catEmpaquesMin.
+	 * @return catEmpaquesMin 
+	 */
+	public float getCatEmpaquesMin() {
+		return this.catEmpaquesMin;
+	}
+
+	/**
+	 * Sets a value to attribute catEmpaquesMin. 
+	 * @param newCatEmpaquesMin 
+	 */
+	public void setCatEmpaquesMin(float newCatEmpaquesMin) {
+		this.catEmpaquesMin = newCatEmpaquesMin;
+	}
+
+	/**
+	 * Returns costoEmpaquetado.
+	 * @return costoEmpaquetado 
+	 */
+	public float getCostoEmpaquetado() {
+		return this.costoEmpaquetado;
+	}
+
+	/**
+	 * Sets a value to attribute costoEmpaquetado. 
+	 * @param newCostoEmpaquetado 
+	 */
+	public void setCostoEmpaquetado(float newCostoEmpaquetado) {
+		this.costoEmpaquetado = newCostoEmpaquetado;
+	}
+
+	/**
+	 * Returns costoBotellaMin.
+	 * @return costoBotellaMin 
+	 */
+	public float getCostoBotellaMin() {
+		return this.costoBotellaMin;
+	}
+
+	/**
+	 * Sets a value to attribute costoBotellaMin. 
+	 * @param newCostoBotellaMin 
+	 */
+	public void setCostoBotellaMin(float newCostoBotellaMin) {
+		this.costoBotellaMin = newCostoBotellaMin;
+	}
+
+}
